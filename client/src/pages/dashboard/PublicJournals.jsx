7c74aa9c855f17908f5436f5d88ed298d900fec3
ayush0402 <lit2020024@iiitl.ac.in>
2023-03-30 05:10:59 +0530

    Added PostCardView component

--- client/src/pages/dashboard/PublicJournals.jsx
+++ client/src/pages/dashboard/PublicJournals.jsx
@@ -1,14 +1,40 @@
+import { useEffect, useState } from "react";
 import DashboardLayout from "../../components/DashboardLayout";
+import axios from "axios";
+import PostCardView from "../../components/PostCardView";
 
 const PublicJournals = () => {
+  const [loading, setLoading] = useState(true);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const { data: response } = await axios.get("/post/all");
+        setPosts(response);
+      } catch (error) {
+        console.error(error.message);
+      }
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <>
-      <DashboardLayout>
-        <div>
-          <h1>Public Journals</h1>
-        </div>
-      </DashboardLayout>
-    </>
+    <DashboardLayout>
+      <div>
+        {loading && <div>Loading</div>}
+        {!loading && (
+          <div>
+            {posts.map((post) => {
+              return <PostCardView post={post} />;
+            })}
+          </div>
+        )}
+      </div>
+    </DashboardLayout>
   );
 };
 
